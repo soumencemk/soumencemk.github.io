c9d2e08d92dbbd0c6573eced7b48badaff71f7ce
Soumen Karmakar <soumen.karmakar@ericsson.com>
2021-09-10 16:35:59 +0100

    Fix form

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -64,7 +64,7 @@
     })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
+      if (data.trim().includes("thanks")) {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
       } else {
